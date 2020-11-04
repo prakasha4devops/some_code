43d9ff33c26e5a4edb54b04afdd3b5f498690a25
Prakash A <prakashadmane@gmail.com>
2020-11-04 15:37:35 +0000

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
+echo "-------------"
 echo "$NUMBER"
 echo "-------------"
 echo "This was added"
 echo "-------------"
+echo '##############'
